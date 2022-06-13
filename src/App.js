14609f8213242e934a9b4d3fa0d099842e2a7a03
dqt392 <tuandqse160189@fpt.edu.vn>
2022-06-13 17:26:32 +0700

    Components Part 3_2

--- src/App.js
+++ src/App.js
@@ -5,50 +5,60 @@ import { Welcome } from "./Components/Welcome";
 import { Comment } from "./Components/Comment";
 import Car from "./Components/Car";
 import Menu from "./Components/MenuComponent";
+import { Component } from "react";
+import { DISHES } from "./shared/dishes";
 
 const comment = {
   date: new Date(),
   text: "I hope you enjoy my restaurant",
   author: { name: "alberto", avatarUrl: "./assets/images/alberto.png" },
 };
-function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+class App extends Component {
+  // return (
+  //   // <div className="App">
+  //   //   <header className="App-header">
+  //   //     <img src={logo} className="App-logo" alt="logo" />
+  //   //     <p>
+  //   //       Edit <code>src/App.js</code> and save to reload.
+  //   //     </p>
+  //   //     <a
+  //   //       className="App-link"
+  //   //       href="https://reactjs.org"
+  //   //       target="_blank"
+  //   //       rel="noopener noreferrer"
+  //   //     >
+  //   //       Learn React
+  //   //     </a>
+  //   //   </header>
+  //   // </div>
 
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand>Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Welcome name="Tuan"></Welcome>
-      <Welcome name="Van"></Welcome>
-      <Welcome name="Thuy van"></Welcome>
+  //   <div className="App">
+  //     <Navbar dark color="primary">
+  //       <div className="container">
+  //         <NavbarBrand>Ristorante Con Fusion</NavbarBrand>
+  //       </div>
+  //     </Navbar>
+  //     <Welcome name="Tuan"></Welcome>
+  //     <Welcome name="Van"></Welcome>
+  //     <Welcome name="Thuy van"></Welcome>
 
-      <Comment
-        date={comment.date}
-        text={comment.text}
-        author={comment.author}
-      ></Comment>
-      <Car></Car>
-      <Menu></Menu>
-    </div>
-  );
+  //     <Comment
+  //       date={comment.date}
+  //       text={comment.text}
+  //       author={comment.author}
+  //     ></Comment>
+  //     <Car></Car>
+  //     <Menu></Menu>
+  //   </div>
+  // );
+  constructor(props) {
+    super(props);
+    this.state = { dishes: DISHES };
+  }
+
+  render() {
+    return <Menu dishes={this.state.dishes}></Menu>;
+  }
 }
 
 export default App;
